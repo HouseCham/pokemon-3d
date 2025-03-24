a857d88aeffdeb9883dabda205bbfe47f38b6df1
HouseCham <ramsesramirezvallejo@gmail.com>
2025-03-24 17:33:08 -0600

    Add DirectionalLight to Lights class for enhanced scene illumination

--- src/classes/Lights.ts
+++ src/classes/Lights.ts
@@ -1,8 +1,12 @@
-import { AmbientLight, HemisphereLight, Scene } from "three";
+import { AmbientLight, DirectionalLight, HemisphereLight, Scene } from "three";
 
 export class Lights {
     constructor(scene: Scene) {
         scene.add(new AmbientLight(0xffffff, 0.8));
         scene.add(new HemisphereLight(0xffffbb, 0.8));
+
+        const sun = new DirectionalLight(0xffffff, 0.5);
+        sun.position.set(20, 20, 5);
+        scene.add(sun);
     }
 };
